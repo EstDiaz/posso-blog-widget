d0b7e4f96f320b5f53d2e18360803fc17cb18bdc
Esteban Díaz <hola@estebandiaz.net>
2026-05-08 00:21:40 +0200

    styles

--- lw-rss-engine.js
+++ lw-rss-engine.js
@@ -157,11 +157,12 @@
             .card-list .js-learnworlds-overlay { position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: var(--blog-color); mix-blend-mode: multiply; pointer-events: none; }
 
             /* CARD-COLOR */
-            .card-color { padding: 16px; }
+            .card-color { padding: 16px; border-radius: 16px;}
             .lw-blog-card .card-color { color: var(--ash-700);  background: var(--salmon-150); }
-            .lw-blog-card:hover .card-color { background: var(--salmon-200); }
+            .lw-blog-card:hover .card-color { background: var(--salmon-200); color: var(--ash-700) }
             .lw-blog-card .card-color .lw-tags { display: flex; gap: 8px; justify-content: flex-end; flex-wrap: wrap; overflow: hidden; }
-            .lw-blog-card .card-color .lw-tag { font-size: 12px; border-radius: 9999px; padding: 8px 16px; white-space: nowrap; line-height: 1; background: var(--salmon-300); color: var(--salmon-150); }
+            .lw-blog-card .card-color .lw-tag { font-size: 12px; border-radius: 9999px; padding: 8px 16px; white-space: nowrap; line-height: 1; background: var(--salmon-300); color: var(--ash-700); }
+            .lw-blog-card:hover .card-color .lw-tag { background: var(--salmon-100) }
             .lw-blog-card .card-color .learnworlds-image { width: calc(100% - 32px); margin: 8px auto; border-radius: 8px; }
             .dark-cards .lw-blog-card { background: var(--blog-color); }
 
